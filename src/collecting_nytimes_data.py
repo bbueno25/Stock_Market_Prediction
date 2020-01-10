9ed33aeb081e7d0034c3a9bd9e5090ed61fc7ae4
B. Bueno <bbueno5000@gmail.com>
2020-01-10 15:51:07 -0700

    edited collecting_nytimes_data.py

--- src/collecting_nytimes_data.py
+++ src/collecting_nytimes_data.py
@@ -1,48 +1,30 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+
 """
 Created on Mon Mar 20 00:10:29 2017
-
 @author: Dinesh
-"""
-########## News API ########################################################
-from newsapi import NewsAPI
 
-key = '96af62a035db45bda517a9ca62a25ac3'
-params = {}
-api = NewsAPI(key)
-sources = api.sources(params)
-articles = api.articles(sources[0]['id'], params)
-
-################ NY Times API #############################################
-
-
-import sys, csv, json
-reload(sys)
-sys.setdefaultencoding('utf8')
-
-
-import requests
-"""
 About:
-Python wrapper for the New York Times Archive API 
+Python wrapper for the New York Times Archive API
 https://developer.nytimes.com/article_search_v2.json
 """
 
-class APIKeyException(Exception):
-    def __init__(self, message): self.message = message 
-
-class InvalidQueryException(Exception):
-    def __init__(self, message): self.message = message 
+import csv
+import json
+from newsapi import NewsAPI
+import requests
+import sys
 
 class ArchiveAPI(object):
+
     def __init__(self, key=None):
         """
         Initializes the ArchiveAPI class. Raises an exception if no API key is given.
         :param key: New York Times API Key
         """
         self.key = key
-        self.root = 'http://api.nytimes.com/svc/archive/v1/{}/{}.json?api-key={}' 
+        self.root = 'http://api.nytimes.com/svc/archive/v1/{}/{}.json?api-key={}'
         if not self.key:
             nyt_dev_page = 'http://developer.nytimes.com/docs/reference/keys'
             exception_str = 'Warning: API Key required. Please visit {}'
@@ -51,6 +33,7 @@ class ArchiveAPI(object):
     def query(self, year=None, month=None, key=None,):
         """
         Calls the archive API and returns the results as a dictionary.
+
         :param key: Defaults to the API key used to initialize the ArchiveAPI class.
         """
         if not key: key = self.key
@@ -62,20 +45,30 @@ class ArchiveAPI(object):
         r = requests.get(url)
         return r.json()
 
+class APIKeyException(Exception):
 
+    def __init__(self, message):
+        self.message = message
+
+class InvalidQueryException(Exception):
+
+    def __init__(self, message):
+        self.message = message
+
+key = '96af62a035db45bda517a9ca62a25ac3'
+params = {}
+api = NewsAPI(key)
+sources = api.sources(params)
+articles = api.articles(sources[0]['id'], params)
+reload(sys)
+sys.setdefaultencoding('utf8')
 api = ArchiveAPI('0ba6dc04a8cb44e0a890c00df88c393a')
-
-
-years = [2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007]
 months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-
+years = [2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007]
 for year in years:
     for month in months:
         mydict = api.query(year, month)
-        file_str = '/Users/Dinesh/Documents/Project Stock predictions/data/nytimes/' + str(year) + '-' + '{:02}'.format(month) + '.json'
+        file_str = 'data/nytimes/' + str(year) + '-' + '{:02}'.format(month) + '.json'
         with open(file_str, 'w') as fout:
             json.dump(mydict, fout)
         fout.close()
-        
-
-    
